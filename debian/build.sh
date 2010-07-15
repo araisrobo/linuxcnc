2711525f2a90fee7bad450e897e94bd3840fc5cc
Yi-Shin Li <ysli@araisrobo.com>
2010-07-15 12:28:23 +0800

    update build.sh, that builds Debian Binary Package

--- debian/build.sh
+++ debian/build.sh
@@ -3,9 +3,8 @@
 #usage: source ./build.sh
 RELEASE=2.4.0-nurbs-2010.07.14
 ./configure sim
-cd ..
 nice debuild -S
-sudo nice pbuilder build ../emc2_${RELEASE}.dsc
+sudo nice pbuilder build ../../emc2_${RELEASE}.dsc
 cp -v \
   /var/cache/pbuilder/result/emc2-sim_${RELEASE}_i386.deb \
   ~/tmp
