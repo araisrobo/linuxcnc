c5502d50e7cae0164f7ef11477005368bbe0e5d6
Eric, Hsu-yao Tsai <hsuyao.tsai@araisrobo.com>
2011-03-06 17:42:48 +0800

    update for release
    Signed-off-by: Eric, Hsu-yao Tsai <hsuyao.tsai@araisrobo.com>

--- debian/build.sh
+++ debian/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #usage: source ./build.sh
-RELEASE=2.4.0-nurbs-2011.01.03
+RELEASE=2.4.0-nurbs-2011.03.06
 ./configure sim
 nice debuild -S
 sudo nice pbuilder build ../../emc2_${RELEASE}.dsc
