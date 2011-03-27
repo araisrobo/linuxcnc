5d9cd88b0b18ab2ab14c2aa5aaf5f42426a10e25
Yi-Shin Li <ysli@araisrobo.com>
2011-03-28 06:35:03 +0800

    Update release version for TAIWAN PLASMA

--- debian/build.sh
+++ debian/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #usage: source ./build.sh
-RELEASE=2.4.0-nurbs-2011.03.23
+RELEASE=2.4.0-nurbs-2011.03.28
 ./configure sim
 nice debuild -S
 sudo nice pbuilder build ../../emc2_${RELEASE}.dsc
