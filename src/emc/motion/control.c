ef962fd464dda2e20564da36acd88967815ce770
Yishin Li <ysli@araisrobo.com>
2011-08-19 14:55:12 +0800

    clean up code

--- src/emc/motion/control.c
+++ src/emc/motion/control.c
@@ -906,6 +906,7 @@ static void check_for_faults(void)
     /* only check enable input if running */
     if ( GET_MOTION_ENABLE_FLAG() != 0 ) {
 	if ( *(emcmot_hal_data->enable) == 0 ) {
+            int n;
 	    reportError(_("motion stopped by enable input"));
 	    emcmotDebug->enabling = 0;
 
