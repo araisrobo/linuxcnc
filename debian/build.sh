48eea1026b86cbb6108e1d85ed583d44ed6d1886
Yishin Li <ysli@araisrobo.com>
2011-12-11 15:18:40 +0800

    Update binary release revision -- 2011.12.11

--- debian/build.sh
+++ debian/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #usage: source ./build.sh
-RELEASE=2.4.0-nurbs-2011.11.17
+RELEASE=2.4.0-nurbs-2011.12.11
 ./configure sim
 nice debuild -S
 sudo nice pbuilder build ../../emc2_${RELEASE}.dsc
