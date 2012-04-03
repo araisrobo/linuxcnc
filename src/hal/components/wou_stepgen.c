f6994ba7ab8783c3fe2c9e0bcfd8eebfee2a1418
Eric, Hsu-yao Tsai <hsuyao.tsai@araisrobo.com>
2012-04-03 10:38:05 +0800

    UPDATE: remove HAL pin: joint_cmd         add HAL pin: cmd_fbs, cmf_fbf to retrieve the position command which remains in RISC.

--- src/hal/components/wou_stepgen.c
+++ src/hal/components/wou_stepgen.c
@@ -404,7 +404,9 @@ typedef struct {
     int32_t motion_type;          /* motion type wrote to risc */
     
     /* debug info */
-    hal_s32_t   *joint_cmd;       /* increased pulse counts of this BP */
+    // hal_s32_t   *joint_cmd;       /* increased pulse counts of this BP */
+    hal_s32_t   *cmd_fbs;       /* increased pulse counts of this BP */
+    hal_float_t   *cmd_fbf;       /* increased pulse counts of this BP */
 
 } stepgen_t;
 
@@ -613,7 +615,9 @@ static void fetchmail(const uint8_t *buf_head)
                 //obsolete: *(machine_control->ferror[i]) = (int32_t)*p;
                 // joint_cmd of this BP
                 p += 1;
-                *(stepgen->joint_cmd) = ((int32_t)*p);
+//                *(stepgen->joint_cmd) = ((int32_t)*p);
+                *(stepgen->cmd_fbs) = ((int32_t)*p);
+                *(stepgen->cmd_fbf) = ((int32_t)*p) * (stepgen->scale_recip);
 
                 stepgen += 1;   // point to next joint
             //obsolete: } else {
@@ -2243,8 +2247,18 @@ static int export_stepgen(int num, stepgen_t * addr,/* obsolete: int step_type,*
     // rtapi_set_msg_level(RTAPI_MSG_ALL);
     
     /* debug info */
-    retval = hal_pin_s32_newf(HAL_OUT, &(addr->joint_cmd), comp_id,
-			      "wou.stepgen.%d.joint_cmd", num);
+//    Encryptedretval = hal_pin_s32_newf(HAL_OUT, &(addr->joint_cmd), comp_id,
+//			      "wou.stepgen.%d.joint_cmd", num);
+//    if (retval != 0) {
+//	return retval;
+//    }
+    retval = hal_pin_s32_newf(HAL_OUT, &(addr->cmd_fbs), comp_id,
+			      "wou.stepgen.%d.cmd-fbs", num);
+    if (retval != 0) {
+	return retval;
+    }
+    retval = hal_pin_float_newf(HAL_OUT, &(addr->cmd_fbf), comp_id,
+			      "wou.stepgen.%d.cmd-fbf", num);
     if (retval != 0) {
 	return retval;
     }
