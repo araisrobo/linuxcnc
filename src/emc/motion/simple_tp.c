42b94936b2a7163f28a72eb0c0fd6fe3fe9d6bce
Yishin Li <ysli@araisrobo.com>
2012-08-26 16:42:50 +0800

    BUGFIX to jerk limitation for simple_tp.c

--- src/emc/motion/simple_tp.c
+++ src/emc/motion/simple_tp.c
@@ -22,7 +22,7 @@
 #include "posemath.h"
 
 // to disable DP(): #define TRACE 0
-#define TRACE 1
+#define TRACE 0
 #include "dptrace.h"
 #if (TRACE!=0)
 // FILE *dptrace = fopen("dptrace.log","w");
@@ -44,40 +44,49 @@ static uint32_t dt = 0;
 
 void simple_tp_update(simple_tp_t *tp, double period)
 {
-    double max_dv, tiny_dp, pos_err, vel_req, vel_req_cmd;
+    double max_dv, tiny_dp, pos_err, vel_req;
     double max_da;
     double dv_upper, dv_lower;
+    double acc_req;
+    double vel_err;
+    double tiny_dv;
+
+#if (TRACE!=0)
+    double vel_req_cmd;
+
+    if (dptrace == 0) {
+        dptrace = fopen("simple_tp.log","w");
+        dt = 0;
+        DPS("%11s  %15s%15s%15s%15s%15s%15s%15s%15s%7s%7s ",
+             "#dt",  "pos_cmd", "curr_pos", "curr_vel", "curr_acc", "max_dv", "vel_req", "max_vel", "max_acc", "enable", "active"
+           );
+    }
+#endif
 
     tp->active = 0;
     /* compute max change in velocity per servo period */
+    max_dv = tp->max_acc * period;                  // unit: dist/sec
+    max_da = tp->max_jerk * period;                 // max_da: unit: dist/sec^2
     if (tp->position_mode) {
-        max_dv = tp->max_acc * period;
         dv_upper = max_dv;
         dv_lower = -max_dv;
     } else {
         // s-curve for non-positioning motion only
-        double max_acc_dt;
-        max_acc_dt = (tp->max_acc * period);
-        max_da = tp->max_jerk * period;
         // this max_dv is buggy; it's just for T-curve vel_req calc
-        max_dv = fabs(tp->curr_acc) + max_da;  //curr_acc: unit/base_period
-        if (max_dv > max_acc_dt) {
-            max_dv = max_acc_dt;
+        dv_upper = (tp->curr_acc + max_da) * period;    // dv_upper: unit: dist/sec
+        dv_lower = (tp->curr_acc - max_da) * period;
+        if (dv_upper > max_dv) {
+            dv_upper = max_dv;
         }
-        dv_upper = tp->curr_acc + max_da;
-        dv_lower = tp->curr_acc - max_da;
-        if (dv_upper > max_acc_dt) {
-            dv_upper = max_acc_dt;
-        }
-        if (dv_lower < -max_acc_dt) {
-            dv_lower = -max_acc_dt;
+        if (dv_lower < -max_dv) {
+            dv_lower = -max_dv;
         }
     }
         
     /* compute a tiny position range, to be treated as zero */
-    tiny_dp = max_dv * period * 0.001;
     /* calculate desired velocity */
     if (tp->enable) {
+        tiny_dp = max_dv * period * 0.001;
         /* accurately positioning at "tp->pos_cmd" */
         /* planner enabled, request a velocity that tends to drive
            pos_err to zero, but allows for stopping without position
@@ -107,47 +116,88 @@ void simple_tp_update(simple_tp_t *tp, double period)
 	tp->pos_cmd = tp->curr_pos;
     }
 
+#if (TRACE!=0)
     vel_req_cmd = vel_req;
+#endif
 
-    /* limit velocity request */
-    if (vel_req > tp->max_vel) {
-        vel_req = tp->max_vel;
-    } else if (vel_req < -tp->max_vel) {
-	vel_req = -tp->max_vel;
-    }
-    /* ramp velocity toward request at accel limit */
-    if (vel_req > tp->curr_vel + dv_upper) {
-        vel_req = tp->curr_vel + dv_upper;
-    } else if (vel_req < tp->curr_vel + dv_lower) {
-        vel_req = tp->curr_vel + dv_lower;
-    }
-    tp->curr_acc = (vel_req - tp->curr_vel);
-    tp->curr_vel = vel_req;
+    if (tp->position_mode == 1) {
+        // accurate T-curve positioning
+        /* ramp velocity toward request at accel limit */
+        if (vel_req > tp->curr_vel + dv_upper) {
+            vel_req = tp->curr_vel + dv_upper;
+        } else if (vel_req < tp->curr_vel + dv_lower) {
+            vel_req = tp->curr_vel + dv_lower;
+        }
+        /* limit velocity request */
+        if (vel_req > tp->max_vel) {
+            vel_req = tp->max_vel;
+        } else if (vel_req < -tp->max_vel) {
+            vel_req = -tp->max_vel;
+        }
+        tp->curr_acc = (vel_req - tp->curr_vel) / period;
+        tp->curr_vel = vel_req;
+    } else {
+        // s-curve for non-positioning motion only
+        /* compute a tiny velocity range, to be treated as zero */
+        tiny_dv = max_da * period * 0.001;
+        vel_err = vel_req - tp->curr_vel;
+        if (vel_err > tiny_dv) {
+            acc_req = -max_da +
+                       sqrt(2.0 * tp->max_jerk * vel_err + max_da * max_da);
+        } else if (vel_err < -tiny_dv) {
+            // vel_req <= 0
+            acc_req =  max_da -
+                       sqrt(-2.0 * tp->max_jerk * vel_err + max_da * max_da);
+        } else {
+            acc_req = 0;  // acc_req is within tiny_dv
+        }
 
+#if (TRACE!=0)
+        if ((dt % 5) == 0) {        // check for j0 of 5-joints
+            DP("\nvel_err(%f) acc_req(%f)\n", vel_err, acc_req);
+        }
+#endif
+
+        /* limit accel toward request at jerk limit */
+        if (acc_req > tp->curr_acc + max_da) {
+            acc_req = tp->curr_acc + max_da;
+        } else if (acc_req < tp->curr_acc - max_da) {
+            acc_req = tp->curr_acc - max_da;
+        }
+
+        /* limit accel request */
+        if (acc_req > tp->max_acc) {
+            tp->curr_acc = tp->max_acc;
+        } else if (acc_req < -tp->max_acc) {
+            tp->curr_acc = -tp->max_acc;
+        } else {
+            tp->curr_acc = acc_req;
+        }
+
+        tp->curr_vel += (tp->curr_acc * period);
+
+        /* check for still moving */
+        if (fabs(tp->curr_vel) <= tiny_dv) {
+            tp->curr_vel = 0;
+        }
+    }
 
     /* check for still moving */
     if (tp->curr_vel != 0.0) {
 	/* yes, mark planner active */
 	tp->active = 1;
     }
+
     /* integrate velocity to get new position */
     tp->curr_pos += tp->curr_vel * period;
 
 #if (TRACE!=0)
-    if (dptrace == 0) {
-        dptrace = fopen("simple_tp.log","w");
-        dt = 0;
-        DPS("%11s  %15s%15s%15s%15s%15s%15s%15s%7s%7s ",
-             "#dt",  "pos_cmd", "curr_pos", "curr_vel", "curr_acc", "max_dv", "vel_req", "max_vel", "enable", "active"
+    if ((dt % 5) == 0) {        // check for j0 of 5-joints
+        DPS("\n%11u ", dt);
+        DPS(" %15.7f%15.7f%15.7f%15.7f%15.7f%15.7f%15.7f%15.7f%7d%7d",
+              tp->pos_cmd, tp->curr_pos, tp->curr_vel, tp->curr_acc, max_dv, vel_req_cmd, tp->max_vel, tp->max_acc, tp->enable, tp->active
            );
     }
-
-    if ((dt % 5) == 0) {        // check for j0 of 5-joints
-        DPS("\n%11u ", dt >> 2);    
-    }
-    DPS(" %15.7f%15.7f%15.7f%15.7f%15.7f%15.7f%15.7f%7d%7d ",
-          tp->pos_cmd, tp->curr_pos, tp->curr_vel, tp->curr_acc, max_dv, vel_req_cmd, tp->max_vel, tp->enable, tp->active
-       );
     dt += 1;
 #endif
 
