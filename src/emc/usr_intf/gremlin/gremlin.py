71a4e96e6a9661ddba119ce149eab8f317a8a5cf
WH Zheng <ddtrng@araisrobo.com>
2013-10-14 09:18:05 +0800

    if open not converted_dryrun will return
    * prevent display jump other file

--- src/emc/usr_intf/gremlin/gremlin.py
+++ src/emc/usr_intf/gremlin/gremlin.py
@@ -239,6 +239,10 @@ class Gremlin(gtk.gtkgl.widget.DrawingArea, glnav.GlNavBase,
 
         td = tempfile.mkdtemp()
         self._current_file = filename
+#        print 'gremlin.load(%s)' % filename
+        if ('converted_dryrun') not in filename:
+#            print 'no load remap, but it can run, just no display'
+            return
         try:
             random = int(self.inifile.find("EMCIO", "RANDOM_TOOLCHANGER") or 0)
             # we want canon to retain some information
